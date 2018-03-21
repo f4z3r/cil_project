f8e3009fc2c8d4e01ef370e54684f9fa2bb5c9c8
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-03-21 20:59:59 +0100

    Added test set augmentation function

--- src/utility.py
+++ src/utility.py
@@ -7,11 +7,46 @@
 import numpy as np
 import os
 import sys
-from PIL import Image
+import glob
+import matplotlib.image as mpimg
 
-def load_image(filename):
-    """Loads an image from disk and return it."""
-    return Image.open(filename).load()
+def load_image(filepath):
+    """Loads an image from disk and return it as a 2D pixel array.
+
+    Args:
+        filename (str): full path to image to be loaded
+
+    Returns:
+        numpy.ndarray : Array containing the pixel colour information.
+    """
+    return mpimg.imread(filepath)
+
+def augment_img_set(path):
+    """Augments the set of test images via rotations and reflections.
+
+    Args:
+        path (str): path to image folder.
+
+    Returns:
+        Nothing.
+    """
+    for file in glob.glob(os.path.join(path, "*.jpg")):
+        filename_no_ext = "".join(os.path.basename(file).split(".")[:-1])
+        filepath_no_ext = os.path.join(path, filename_no_ext)
+        img = load_image(file)
+        cpy_img = img
+
+        # Generate 3 rotated copies
+        for idx in range(3):
+            cpy_img = np.rot90(cpy_img)
+            mpimg.imsave("{}_{}.jpg".format(filepath_no_ext, idx), cpy_img)
+
+        # Generate 4 rotated and flipped copies
+        flip_img = np.flipud(img)
+        for idx in range(3, 7):
+            flip_img = np.rot90(flip_img)
+            mpimg.imsave("{}_{}.jpg".format(filepath_no_ext, idx), flip_img)
+
 
 
 
