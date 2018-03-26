d3e423cc976e106a94837e46e139730d87544cbb
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-03-26 11:06:24 +0200

    Start #12

---
+++ src/models/dnn_classifier.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+
+import os, sys, logging
+import numpy as np
+import tensorflow as tf
+
+logger = logging.getLogger("cil_project.models.dnn_classifier")
+
+class Model:
+    """Deep neural network classifier model. This is composed of several fully connected layers."""
+    def __init__(self):
+        """Initialise the model.
+
+        Args:
+            - pass
+        """
+        logger.info("Generating DNN classifier ...")
+        pass
