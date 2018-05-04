9c837e6f07308915b26bd22843c48a569287805f
Robin <xsirobin@gmail.com>
2018-05-04 14:28:07 +0200

    Fixed bugs in patch generator

--- src/generators/PatchTrainImageGenerator.py
+++ src/generators/PatchTrainImageGenerator.py
@@ -1,16 +1,17 @@
 import glob
-import os, logging
-import keras
-import scipy
+import logging
+import os
 
+import keras
 import matplotlib.image as mpimg
 import numpy as np
+import scipy
 
 logger = logging.getLogger("cil_project.src.generators.PatchTrainImageGenerator")
 
 
 class PatchTrainImageGenerator:
-    def __init__(self, path_to_images, path_to_groundtruth, pad=28, patch_size=16, context_padding=28):
+    def __init__(self, path_to_images, path_to_groundtruth, pad=28, patch_size=16):
         data_files = glob.glob(os.path.join(path_to_images, "*.png"))
         mask_files = glob.glob(os.path.join(path_to_groundtruth, "*.png"))
         image_count = len(data_files)
@@ -26,32 +27,29 @@ class PatchTrainImageGenerator:
             data_set[idx] = np.pad(mpimg.imread(file), ((pad, pad), (pad, pad), (0, 0)), mode="reflect")
             verifier_set[idx] = mpimg.imread(mask_file)
 
+        verifier_set = verifier_set / verifier_set.max()  # normalize data
+        print(verifier_set.min(), verifier_set.max())
+
         self.data_set = data_set
         self.verifier_set = verifier_set
 
-        self.window_size = patch_size + 2 * context_padding
+        self.window_size = patch_size + 2 * pad
         self.patch_size = patch_size
-        self.context_padding = context_padding
 
         logger.info('PatchImageGenerator initialized with {} pictures'.format(image_count))
 
-    def get_random_image_patch(self, data_img, verifier_img, size, stride, pad):
-        h = (np.random.choice(data_img.shape[1] - 2 * pad) // stride) * stride + pad
-        w = (np.random.choice(data_img.shape[0] - 2 * pad) // stride) * stride + pad
+    def get_random_image_patch(self, data_img, verifier_img, size, stride):
+        h = (np.random.choice(verifier_img.shape[1]) // stride) * stride
+        w = (np.random.choice(verifier_img.shape[0]) // stride) * stride
 
-        if len(data_img.shape) == 3:
-            data_patch = data_img[h - pad:h + size + pad, w - pad:w + size + pad, :]
-        else:
-            data_patch = data_img[h - pad:h + size + pad, w - pad:w + size + pad]
-
-        verifier_patch = verifier_img[h - pad:h + size - pad, w - pad:w + size - pad]
+        data_patch = data_img[h:h + size, w:w + size, :]
+        verifier_patch = verifier_img[h:h + stride, w:w + stride]
 
         return data_patch, verifier_patch
 
     def generate_patch(self, batch_size=100, four_dim=False, augmentation=True):
         window_size = self.window_size
         patch_size = self.patch_size
-        context_padding = self.context_padding
         while True:
             batch_data = np.empty((batch_size, window_size, window_size, 3))
             batch_verifier = np.empty((batch_size, 2))
@@ -59,23 +57,21 @@ class PatchTrainImageGenerator:
             for idx in range(batch_size):
                 img_num = np.random.choice(self.data_set.shape[0])
                 image = self.data_set[img_num]
-                groundtruth = self.verifier_set[img_num]
+                ground_truth = self.verifier_set[img_num]
 
                 if augmentation:
                     if np.random.choice([False, True]):
                         # randomly flip
                         image = image[:, ::-1]
-                        groundtruth = groundtruth[:, ::-1]
+                        ground_truth = ground_truth[:, ::-1]
 
                     rotation = np.random.random_sample() * 360
-                    image = scipy.ndimage.interpolation.rotate(image, rotation, mode="nearest", reshape=False)
-                    groundtruth = scipy.ndimage.interpolation.rotate(groundtruth, rotation, mode="nearest", reshape=False)
+                    image = scipy.ndimage.interpolation.rotate(image, rotation, mode="reflect", reshape=False)
+                    ground_truth = scipy.ndimage.interpolation.rotate(ground_truth, rotation, mode="reflect",
+                                                                      reshape=False)
 
-                data_patch, veri_patch = self.get_random_image_patch(image,
-                                                                     groundtruth,
-                                                                     patch_size,
-                                                                     patch_size,
-                                                                     context_padding)
+                data_patch, veri_patch = self.get_random_image_patch(image, ground_truth, window_size, patch_size)
+
                 label = (np.mean(veri_patch) > 0.25) * 1
                 label = keras.utils.to_categorical(label, num_classes=2)
                 batch_data[idx] = data_patch
@@ -85,7 +81,6 @@ class PatchTrainImageGenerator:
                 batch_data = batch_data.reshape(batch_size, self.window_size, self.window_size, 3, 1)
 
             yield (batch_data, batch_verifier)
-
 
     def input_dim(self, four_dim=False):
         if four_dim:
